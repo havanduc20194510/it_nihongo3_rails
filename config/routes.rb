f580e74e72fb2667c88585c13a74890ea060ee71
VanDucKun <havanducdeptrai@gmail.com>
2023-01-30 05:00:32 +0000

    add_home modifier

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+    
   get 'static_pages/home'
+  root 'static_pages#home'
+  
   devise_for :users
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
